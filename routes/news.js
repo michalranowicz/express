52bcf5d2de70c41cfee4eaff7c931a5c45583796
Michał Ranowicz <michal.ranowicz@gmail.com>
2021-02-22 16:32:01 +0100

    menu item

---
+++ routes/news.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res)=> {
+  res.render('news', { title: 'news' });
+});
+
+module.exports = router;
